b5dc5982e46e98ea7d116279f4f4ab5c54a8ddcd
Donttblink <jonathon.lerose@gmail.com>
2026-06-22 16:47:36 -0400

    Created the initial div draw function and added borders on the divs to track

--- js.js
+++ js.js
@@ -1,1 +1,14 @@
 const container = document.querySelector("#container");
+let gridSize = 16;
+
+
+function gridDraw (gridSize) {
+  for (let i = 0; i <= gridSize; i++) { 
+    let square = document.createElement("div");
+    square.classList.add("gridSquare");
+    container.appendChild(square);    
+
+  }
+};
+
+gridDraw(gridSize);
